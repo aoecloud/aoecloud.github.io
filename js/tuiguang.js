70d7b326c506dbda44b329a3beeed1ee771b2a7f
aoeyun <37470502+aoecloud@users.noreply.github.com>
2023-03-21 08:34:51 +0800

    Delete tuiguang.js

--- js/tuiguang.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?fbcba976f2c262434ccdc59b7ba00713";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
